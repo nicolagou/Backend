775b378913b1f797294caa0fba397c315f1cf0b4
Nicolas Lagouarde <anlagouarde@gmail.com>
2023-03-02 14:45:16 -0300

    Desafio corregido

--- desafio1.js
+++ desafio1.js
@@ -2,15 +2,9 @@ let id = 0;
 
 
 class ProductManager {
-    constructor(title, description, price, thumbnail, code, stock, id) {
+    constructor() {
         this.products = [],
-            this.title = title,
-            this.description = description,
-            this.price = price,
-            this.thumbnail = thumbnail,
-            this.code = code,
-            this.stock = stock,
-            this.id = id;
+        this.id = 0;
     }
 
     addProduct(product) {
@@ -19,9 +13,8 @@ class ProductManager {
                 return console.error('Todos los campos son obligatorios');
             } else if (element.code === product.code) {
                 return console.error('El producto con este codigo ya existe');
-            } else {
-                
-            }
+            } 
+            
         }
         product.id = ++id;
         this.products.push(product);
@@ -49,19 +42,19 @@ class ProductManager {
 const producto1 = new ProductManager();
 
 console.log('Lista vacia');
-console.log(producto1.getProducts()); 
+console.log(producto1.getProducts());
 
 console.log('Agrego un producto');
-console.log(producto1.addProduct({title:'titulo1', description: 'descripcion1',price:1000,thumbnail:'sin imagen',code:'1234', stock:2})); 
+console.log(producto1.addProduct({ title: 'titulo1', description: 'descripcion1', price: 1000, thumbnail: 'sin imagen', code: '1234', stock: 2 }));
 
 console.log('Lista con producto1');
-console.log(producto1.getProducts()); 
+console.log(producto1.getProducts());
 
 console.log('Lista con campo vacio');
-console.log(producto1.addProduct({title:'', description: 'descripcion1',price:1000,thumbnail:'sin imagen',code:'1234', stock:2})); 
+console.log(producto1.addProduct({ title: '', description: 'descripcion1', price: 1000, thumbnail: 'sin imagen', code: '1234', stock: 2 }));
 
 console.log('Agrego un producto con codigo repetido');
-console.log(producto1.addProduct({title:'titulo1', description: 'descripcion1',price:1000,thumbnail:'sin imagen',code:'1234', stock:2})); 
+console.log(producto1.addProduct({ title: 'titulo1', description: 'descripcion1', price: 1000, thumbnail: 'sin imagen', code: '1234', stock: 2 }));
 
 console.log('Busco un producto por id y funciona');
 console.log(producto1.getProductById(producto1.products[0].id));
@@ -70,7 +63,7 @@ console.log('Busco un producto por id y no funciona');
 console.log(producto1.getProductById(1235));
 
 console.log('Agrego un producto');
-console.log(producto1.addProduct({title:'titulo2', description: 'descripcion2',price:200,thumbnail:'sin imagen',code:'1235', stock:22})); 
+console.log(producto1.addProduct({ title: 'titulo2', description: 'descripcion2', price: 200, thumbnail: 'sin imagen', code: '1235', stock: 22 }));
 
 console.log('Lista con producto2 agregado');
 console.log(producto1.getProducts()); 
